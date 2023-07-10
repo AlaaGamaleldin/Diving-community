33d368b1437a2e30616a6e3ea9e4687151bcd463
Alaa Gamal <alaadesigner85@gmail.com>
2023-07-10 17:50:57 +0300

    the second commit with fixing the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from '../Rout/app-routing.module';
+import { AppRoutingModule } from './Rout/app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommunityComponent } from './community/community.component';
